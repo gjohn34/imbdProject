7e0919a72348884b2fe34e5d31dbaa6f5b689d28
Glen Johnson <glenjohnson39@yahoo.com.au>
2021-09-07 11:38:05 +1000

    getall directors w/ foreign key

--- imbdDatabase/dbo/Stored Procedures/spGetAllDirectors.sql
+++ imbdDatabase/dbo/Stored Procedures/spGetAllDirectors.sql
@@ -2,5 +2,7 @@
 AS
 BEGIN
 	set nocount on;
-	SELECT * from dbo.Directors;
+	select [d].[Id], [d].[FirstName], [d].[LastName], [d].[CreatedAt], [d].[UpdatedAt], 
+	[m].[Id], [m].[Title], [m].[ReleaseYear], [m].[CreatedAt], [m].[UpdatedAt] 
+	from dbo.Directors as d INNER JOIN dbo.Movies as m ON d.Id = m.DirectorId
 END
--- imdbAPI.Library/DataAccess/DirectorData.cs
+++ imdbAPI.Library/DataAccess/DirectorData.cs
@@ -12,7 +12,21 @@ namespace imdbAPI.Library.DataAccess
     {
         public static List<DirectorDataModel> GetAll()
         {
-            return SqlDataAccess.LoadData<DirectorDataModel, dynamic>("spGetAllDirectors", new { }, "imbdDatabaseConnection");
+            Dictionary<int, DirectorDataModel> DirectorDictionary = new Dictionary<int, DirectorDataModel>();
+
+            return SqlDataAccess.LoadWithForeign<DirectorDataModel, MovieDataModel, dynamic>("spGetAllDirectors",
+                (director, movie) =>
+                {
+                    DirectorDataModel directorEntity;
+                    if (!DirectorDictionary.TryGetValue(director.Id, out directorEntity))
+                    {
+                        directorEntity = director;
+                        DirectorDictionary.Add(directorEntity.Id, directorEntity);
+                    }
+                    directorEntity.movies.Add(movie);
+                    return directorEntity;
+                }, new { }, "Id", "imbdDatabaseConnection");
+                //<DirectorDataModel, dynamic>("spGetAllDirectors", new { }, "imbdDatabaseConnection");
         }
 
         public static void Insert(DirectorDataModel director)
@@ -22,13 +36,13 @@ namespace imdbAPI.Library.DataAccess
         public static DirectorDataModel GetOne(int id)
         {
 
-            var DirectorDictionary = new Dictionary<int, DirectorDataModel>();
+            Dictionary<int, DirectorDataModel> DirectorDictionary = new Dictionary<int, DirectorDataModel>();
 
             return SqlDataAccess.LoadWithForeign<DirectorDataModel, MovieDataModel, dynamic>("spGetMovieById", 
                 (director, movie) => 
                 { 
                     DirectorDataModel directorEntity;
-                    if (!DirectorDictionary.TryGetValue(movie.DirectorId, out directorEntity))
+                    if (!DirectorDictionary.TryGetValue(director.Id , out directorEntity))
                     {
                         directorEntity = director;
                         DirectorDictionary.Add(directorEntity.Id, directorEntity);
